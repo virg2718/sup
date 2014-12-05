6b4f73c35a315794af94082e416cf1cc430f3bf1
stonehilla <stonehilla@gmail.com>
2014-12-05 17:17:41 -0500

    Updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,10 @@ class EventsController < ApplicationController
 		render action: 'home'
 		@groups = Group.find_by_id(params[:id]).order(:moniker)
 	end
+<<<<<<< Updated upstream
 	def new_event
 		@event = Event.new
 	end
+=======
+>>>>>>> Stashed changes
 end
