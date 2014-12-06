7dc386c436e86e51eca3b3f683ddd9f355ca902e
Virgil Marais <virgil.marais@gmail.com>
2014-12-06 00:00:00 -0500

    Corrections

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,8 @@
 module UsersHelper
-<<<<<<< HEAD
+
 	# Calls memberships, sorted by group name
  	def memberships
  		@memberships = Membership.groups.where("user_id = ?", params[:id]).order('moniker')
   	end
-=======
->>>>>>> session
+
 end
