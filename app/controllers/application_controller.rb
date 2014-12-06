5b6ebc8e9580252363e259c0c5e8454cb6e858df
stonehilla <stonehilla@gmail.com>
2014-12-05 23:46:04 -0500

    updates2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-<<<<<<< HEAD
+  
   	protect_from_forgery with: :exception
-	def update_groups
-	end
 	allgroups()
-
-=======
   protect_from_forgery with: :exception
   include SessionsHelper
-<<<<<<< HEAD
-=======
->>>>>>> session
->>>>>>> 10a9dcdd80aaa13f058739e8e73aef77ff5d01bb
 end
