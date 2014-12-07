f6b843da00514ad31629226325e782c9a2320f55
Virgil Marais <virgil.marais@gmail.com>
2014-12-07 09:58:04 -0500

    Changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
 	def show
-		@user = User.find_by_id(params[:id]).order('name')
+#		@user = User.find_by_id(params[:id]).order('name')
 #		def user_params
 #      		params.require(:user).permit(:content, :picture)
-#    	end
-		render action: 'myprofile'
-		memberships()
-    	@newgroups = @group - @memberships
-	end
+    	end
+	
+
 #	def show
 #		render action: 'home'
 #		@events = events
-	end
-	def newmem
-		nm = memberships.new(user_id: params[:user_id])
-		params[:nm][group_id]
+#	end
+#	def newmem
+#		nm = memberships.new(user_id: params[:user_id])
+#		params[:nm][group_id]
+#	end
+
+	def new
+		@user = User.new
 	end
 
 end
